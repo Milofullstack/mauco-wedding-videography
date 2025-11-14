68d907c693a144a9fc042dd6b9483e79dbbae9b0
Milofullstack <124797907+Miloksky@users.noreply.github.com>
2025-11-14 10:58:48 +0100

    Fix: Next/Image using fill to resolve INVALID_IMAGE_OPTIMIZE_REQUEST2

--- components/About.tsx
+++ components/About.tsx
@@ -9,8 +9,7 @@ export default function About(){
             <Image
               src="/images/mauco1.jpg"
               alt="personal photor"
-              width={900}
-              height={1600}
+              fill
               sizes="100vw"
               className="object-cover w-full h-full object-top shadow-2xl"
             ></Image>
